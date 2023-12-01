7e532fedabaa5942727937616592024bb5112394
fengmm <fengmm@docbook.com.cn>
2023-12-01 19:15:50 +0800

    增加favicon

--- src/models/product.js
+++ src/models/product.js
@@ -1,27 +1,31 @@
 import * as blogic from '../blogic'
 const axios = blogic.axios
 
-function toProduct(datas) {
-    let products = [];
-    for(var data in datas) {
-        products.push(new Product(data))
-    }
-    return products
-}
 /* model */
 class Product {
     constructor(arg) {
+        console.log("product", arg)
         this.id = arg.id
         this.companyId = arg.companyId
         this.productName = arg.productName
         this.productDesc = arg.productDesc
         this.createUserId = arg.createUserId
+        this.createUserName = arg.createUserName
         this.createTime = arg.createTime
         this.updateTime = arg.updateTime
     }
     static findByCompanyId(companyId, pageNum, pageSize) {
         return axios.get('/Companies/' + companyId + '/Products?pageSize=1000&pageNum=1')
     }
+
+    static toProduct(datas) {
+        let products = [];
+        for(var data in datas) {
+            products.push(new Product(data))
+        }
+        return products
+    }
+    
 }
 
 export {
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,6 +1,21 @@
 <script setup>
-import { ArrowRight } from '@element-plus/icons-vue'
+import {ref, onMounted} from 'vue'
+import {Product} from '../models/product'
+import * as blogic from '../blogic'
 
+const products = ref([])
+onMounted(() => {
+    loadProducts()
+})
+async function loadProducts() {
+    let context = blogic.loadContext()
+    let productRes = await Product.findByCompanyId(context.currentCompany.companyId, 1, 10)
+    if(productRes?.code === 0){
+        products.value = Product.toProduct(productRes.data)
+    }else {
+        productRes?.showCodeDesc()
+    }
+}
 </script>
 <template>
     <MainContainer>
@@ -9,6 +24,27 @@ import { ArrowRight } from '@element-plus/icons-vue'
             </CompanyProductSelection>
         </template>
         <template #default>
+            <div>
+                <el-button plain>我参与的</el-button>
+                <el-button plain>我创建的</el-button>
+            </div>
+            <div>
+
+            </div>
+            <div style="padding-top: 20px">
+                <el-table :data="products" border style="width: 100%">
+                    <el-table-column prop="id" label="产品id"/>
+                    <el-table-column prop="productName" label="产品名称"/>
+                    <el-table-column prop="createUserName" label="由谁创建" />
+                    <el-table-column prop="createTime" label="创建时间" />
+                    <el-table-column prop="updateTime" label="修改时间" />
+                    <el-table-column prop="id" label="操作">
+                        <template #default>
+                            <el-button text>编辑</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
         </template>
     </MainContainer>
 </template>
